3d8c6c37f7169e900ecf2ae647c5b493542bbdea
chiho@vk.com <iwillhoodoo95>
2022-08-15 14:22:52 +0300

    solved exercises from 2.4 to 2.9

--- PYTHON CRASH COURSE.py
+++ PYTHON CRASH COURSE.py
@@ -1,4 +1,27 @@
 # by Eric Matthes
 # PYTHON
 # CRASH COURSE2ND EDITIONA Hands-On, Project-Based Introduction to Programming
-
+# practice 2.4
+greeting = '«Hello Eric, would you like to learn some Python today?”'
+print(greeting)
+print(greeting.lower())
+print(greeting.upper())
+print(greeting.title())
+# practice 2.5
+print('Albert Einstein once said, "A person who never made \na mistake never tried anything new."')
+# practice 2.6
+famous_person = 'Albert Einstein'
+message = 'A person who never made a mistake never tried anything new.'
+print(f'{famous_person}once said,{message}')
+# practice 2.7
+name = "\n\t  Akhmed   "
+print(name)
+print(name.lstrip().rstrip().strip())
+# practice 2.8
+print(4 + 4)
+print(12 - 4)
+print(2 * 4)
+print(round(16 / 2))
+# practice 2.9
+favorite_number = 23
+print(favorite_number)
