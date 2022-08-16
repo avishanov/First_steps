5248f88f9f8f74e6b40b36f2a2b4266d6a8d9f80
chiho@vk.com <iwillhoodoo95>
2022-08-16 09:24:41 +0300

    solved exercises up to 3.7

--- PYTHON CRASH COURSE.py
+++ PYTHON CRASH COURSE.py
@@ -1,6 +1,6 @@
 # by Eric Matthes
 # PYTHON
-# CRASH COURSE2ND EDITIONA Hands-On, Project-Based Introduction to Programming
+# CRASH COURSE2ND EDITION A Hands-On, Project-Based Introduction to Programming
 # practice 2.4
 greeting = '«Hello Eric, would you like to learn some Python today?”'
 print(greeting)
@@ -25,3 +25,53 @@ print(round(16 / 2))
 # practice 2.9
 favorite_number = 23
 print(favorite_number)
+# practice 3.1
+names = ['Tinky-Winky', 'Dipsy', 'Laa-Laa', 'Po']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+# practice 3.2
+print(f"Hi {names[0]}, what's new?")
+print(f"Hi {names[1]}, what's new?")
+print(f"Hi {names[2]}, what's new?")
+print(f"Hi {names[3]}, what's new?")
+# practice 3.4
+guests = ['Julius Caesar', 'John Wayne', 'William Munny']
+invitation = 'приглашен на обед'
+print(f'{guests[0]} {invitation}')
+print(f'{guests[1]} {invitation}')
+print(f'{guests[2]} {invitation}')
+# practice 3.5
+print(f'{guests[2]} не сможет прийти на обед')
+guests[2] = 'Chingachgook'
+print(f'{guests[0]} {invitation}')
+print(f'{guests[1]} {invitation}')
+print(f'{guests[2]} {invitation}')
+# practice 3.6
+print('список гостей расширен на три человека')
+guests.insert(0, 'Predator')
+guests.insert(2,'Alien')
+guests.append('Terminator')
+print(guests)
+print(f'{guests[0]} {invitation}')
+print(f'{guests[1]} {invitation}')
+print(f'{guests[2]} {invitation}')
+print(f'{guests[3]} {invitation}')
+print(f'{guests[4]} {invitation}')
+print(f'{guests[5]} {invitation}')
+# practice 3.7
+print('на обед приглашаются тлько два гостя')
+del_guest = guests.pop()
+print(f'{del_guest} сожалею об отмене приглашения' )
+del_guest = guests.pop()
+print(f'{del_guest} сожалею об отмене приглашения' )
+del_guest = guests.pop()
+print(f'{del_guest} сожалею об отмене приглашения' )
+del_guest = guests.pop()
+print(f'{del_guest} сожалею об отмене приглашения' )
+print(f'{guests[0]} ранее приглашение остается в силе')
+print(f'{guests[1]} ранее приглашение остается в силе')
+del guests[0]
+del guests[0]
+print(guests)
